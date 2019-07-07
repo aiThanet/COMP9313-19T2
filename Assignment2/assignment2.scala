557d41a18d1c40e4066f7f279b0053194e1bb8cc
Thanet Sirichanyaphong <thanetsirichanyaphong@Thanets-MacBook-Pro.local>
2019-07-07 22:21:36 +1000

    add documentation

--- Assignment2/assignment2.scala
+++ Assignment2/assignment2.scala
@@ -7,17 +7,23 @@ val outputDirPath = "output.csv"
 
 // Write your solution here
 
+// conversion constant
 val MB_to_B = 1048576
 val KB_to_B = 1024
 
+// create an RDD from input file
 val file = sc.textFile(inputFilePath,1)
 
+// split each line by comma(,)
 val lines = file.map(_.split(","))
 
+// transform to a pair of base URL and size of payload RDD
 val pairs = lines.map(x => (x.head, x.last))
 
+// transform to a pair of base URL and size of payload RDD in bytes RDD
 val bytes_pairs = pairs.map(x => if (x._2.takeRight(2) == "MB") (x._1,x._2.dropRight(2).toInt*MB_to_B) else if (x._2.takeRight(2) == "KB") (x._1,x._2.dropRight(2).toInt*KB_to_B)  else (x._1,x._2.dropRight(1).toInt))
 
+// create a case class for calculating min, max, mean and variance
 case class Stats(var total: Int, var min: Int, var max: Int, var count: Int, var all_sizes: List[Int]) {
   def agg(other: Stats): Stats = Stats(
     total + other.total,
@@ -33,10 +39,13 @@ case class Stats(var total: Int, var min: Int, var max: Int, var count: Int, var
   }
 }
 
+// Map size of payload to Stats Object and Reduce with aggregation function in case class
 val result  = bytes_pairs.mapValues(f=>Stats(f,f,f,1,List(f))).reduceByKey(_ agg _)
 
+// Generate final output
 val final_result  = result.map(x=>x._1+","+x._2.gen_output).collect()
 
+// Write to File
 val output_file = new File(outputDirPath)
 
 val bw = new BufferedWriter(new FileWriter(output_file))
